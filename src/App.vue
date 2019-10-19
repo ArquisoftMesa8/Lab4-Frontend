1bffdb7f966bda0cdb856bed78ccfabd02ee02b4
Nicolai Romero <anromerom@unal.edu.co>
2019-10-19 18:54:43 -0500

    Agregado componente APIResponse

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,37 @@
 <template>
-  <div id="app">
+  <div id="app"> 
+
+  <h1> Laboratorio 4 - Frontend </h1>
   
-  
-  
- 
-  <p>Name: {{ name }}</p>
- 
-  <input v-model="name">
-  <br/>
-  <p>Credits: {{ credits }}</p>
-  <input type="number" v-model="credits">
-  <br/>
-  <p>Professor: {{ professor }}</p>
-  <input v-model="professor">
-  <br/>
+  <p>Nombre: {{ name }}</p>
+  <input v-model="name"> <br/>
+  <p>Creditos: {{ credits }}</p>
+  <input type="number" v-model="credits"> <br/>
+  <p>Profesor: {{ professor }}</p>
+  <input v-model="professor"> <br/>
+
   <button v-on:click= "consumeREST"> Crear con REST </button>
-  <button v-on:click= "consumeGraphQL"> Crear con GraphQL </button>
-    <br/>
-    
-    {{ message }}
+  <button v-on:click= "consumeGraphQL"> Crear con GraphQL </button> <br/>
+
+  <div v-if = "message != null"> <APIResponse v-bind:data = "message"  /> </div>
+  
 
   </div>
 </template>
 
 <script>
+import APIResponse from './components/APIResponse.vue'
 import axios from 'axios'
 
 export default {
     name: 'app',
     components: {
+        APIResponse
   
     },
 
     data: ()=>({
-        message: '',
+        message: null,
         name: "Nombre del curso",
         professor: "Nombre del profesor",
         credits: 4
@@ -41,7 +39,7 @@ export default {
     methods:   {
         consumeREST : async function () {
             
-            const url = "http://3.229.229.156:3000/courses-ms/resources/courses"
+            const url = "http://3.229.229.156:3000/courses-ms/resources/courses"  //Url del microservicio courses-ms
             const response = await axios.post(url,
             {
                 data:
@@ -58,13 +56,13 @@ export default {
             console.log(response)
             if(response && response.status == 201) {
                     
-                    this.message = `Curso creado satisfactoriamente desde Microservicio:\n
-                    
-                     Código: ${response.data.code}
-                     \nNombre: ${this.name},
-                     \nCréditos: ${this.credits}
-                     \nProfesor: ${this.professor}
-                     "`
+                    this.message = {
+                        msg:"Curso creado satisfactoriamente desde Microservicio:",                   
+                        code: response.data.code,
+                        name: this.name,
+                        credits: this.credits,
+                        profesor: this.professor
+                     }
 
                 }
 
@@ -72,20 +70,20 @@ export default {
         consumeGraphQL: async function() {
             
             
-            this.message = "GraphQL"
-            const url = "http://3.229.229.156:5000/graphql/"
+            const url = "http://3.229.229.156:5000/graphql/" //Url del microservicio courses-ms
 
             const response = await axios.post(url, {
                 "query" : `mutation {
                         createCourse(course: {
                             name: "${this.name}"
                             credits: ${this.credits}
-                            professor:  "${this.professor}"
+                            professor: "${this.professor}"
                         })
                     {
-                            name
-                            credits
-                            professor
+                           code
+                           name
+                           credits
+                           professor
                     }
                 }`          
             }).catch(e => console.log('Error: ', e) )
@@ -93,13 +91,13 @@ export default {
             console.log(response)
                     if( response && response.status == 200) {
                     
-                    this.message = `Curso creado satisfactoriamente desde API Gateway:\n
-                    
-                     Código: ${response.data.code}
-                     \nNombre: ${this.name},
-                     \nCréditos: ${this.credits}
-                     \nProfesor: ${this.professor}
-                     "`
+               this.message = {
+                        msg:"Curso creado satisfactoriamente desde Microservicio:",                   
+                        code: response.data.data.createCourse.code,
+                        name: response.data.data.createCourse.name,
+                        credits: response.data.data.createCourse.credits,
+                        professor: response.data.data.createCourse.professor
+                     }
 
                 }
 
---
+++ src/components/APIResponse.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h3 v-bind = "data" >  {{ data.msg }}</h3>
+    <h4  v-bind = "data"> Código:      {{ data.code }}     </h4>
+    <h4  v-bind = "data"> Nombre:      {{ data.name }}     </h4>
+    <h4  v-bind = "data"> Créditos: {{ data.credits }}  </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'APIResponse',
+  props: {
+    data: Object
+  }
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+h3 {
+  margin: 20px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
